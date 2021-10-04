37fb00c4fd6f6161f3125019190b430ab900ac2b
Christian Kulesza <christian.kulesza@waits-gmbh.de>
2021-10-04 17:14:30 +0200

    Roles added to database

---
+++ Abschlussprojekt2021/Services/Roles.cs
@@ -0,0 +1,42 @@
+﻿using Abschlussprojekt2021.Resources;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace Abschlussprojekt2021.Services
+{
+    public class Roles
+    {
+        public static async Task createRoles(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                IdentityResult roleResult;
+                // Adding admin role
+                var roleCheck = await RoleManager.RoleExistsAsync(Constants.Admin);
+                // checking if role already exists
+                if (!roleCheck)
+                {
+                    // create the role and seed them to the database if not exist
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(Constants.Admin));
+                }
+
+                // Adding editor role
+                roleCheck = await RoleManager.RoleExistsAsync(Constants.Editor);
+                // checking if role already exists
+                if (!roleCheck)
+                {
+                    // create the role and seed them to the database if not exist
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(Constants.Editor));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
